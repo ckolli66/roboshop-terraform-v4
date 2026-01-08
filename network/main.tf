49a4daeacf86fb406c1e7efc698af78152e35e22
Chanu <kollichanu@gmail.com>
2026-01-08 16:22:52 -0600

    added terraform network module

---
+++ network/main.tf
@@ -0,0 +1,24 @@
+resource "aws_security_group" "allow-all" {
+  name = "roboshop-allow-all"
+  description = "Allow all traffic"
+
+  egress {
+	from_port        = 0
+	to_port          = 0
+	protocol         = "-1"
+	cidr_blocks      = ["0.0.0.0/0"]
+	ipv6_cidr_blocks = ["::/0"]
+	self = true
+  }
+  ingress {
+	from_port        = 0
+	to_port          = 0
+	protocol         = "-1"
+	cidr_blocks      = ["0.0.0.0/0"]
+	ipv6_cidr_blocks = ["::/0"]
+	self = true
+  }
+  tags = {
+	Name = "allow-all"
+  }
+}
